c67580ea08df291d83e177a83db5082e650a654c
Rizandro_Carvalho <145361320+Rizandrodev@users.noreply.github.com>
2024-09-26 14:58:52 +0100

    Update portscan.py
    adicionando bannergrabling

--- portscan.py
+++ portscan.py
@@ -1,14 +1,21 @@
 import socket
 import sys
 
-portas=[21,22,80,443,8080,3306,3000,5173]
+portas = [21, 22, 80, 443, 8080, 3306, 3000, 5173]
+host = sys.argv[1]
 
-for port in range(1,65536):
+for port in range(1, 65536):
     meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    meusocket.settimeout(0.5) #assincronismo
-    code=meusocket.connect_ex((sys.argv[1],port))
-    # if code=0 porta aberta  se for 111 fechado 
-    if code ==0:
-        print(port,"porta aberta")
-    # else:
-    #     print(code,port)
+    meusocket.settimeout(0.5)
+    code = meusocket.connect_ex((host, port))
+    
+    if code == 0:
+        meusocket.connect((host, port))
+        try:
+            banner = meusocket.recv(1024).decode().replace("\n","")
+        except:
+            banner = "Nao identificado"
+        
+        print(port, banner)
+        
+        meusocket.close()
